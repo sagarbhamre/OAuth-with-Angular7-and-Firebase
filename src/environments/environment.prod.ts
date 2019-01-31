88f6d4408a16c8e39804e2209b4b21643a11658c
Sagar Bhamare <sagar.bhamare@nihilent.com>
2019-01-31 11:57:41 +0530

    removed API details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig:{
-    apiKey: "AIzaSyDgW8FEr6xt060x1TdHz_pHZC6qrMSFB9g",
-    authDomain: "fir-auth-9c195.firebaseapp.com",
-    databaseURL: "https://fir-auth-9c195.firebaseio.com",
-    projectId: "fir-auth-9c195",
-    storageBucket: "fir-auth-9c195.appspot.com",
-    messagingSenderId: "669162847387"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig:{
-    apiKey: "AIzaSyDgW8FEr6xt060x1TdHz_pHZC6qrMSFB9g",
-    authDomain: "fir-auth-9c195.firebaseapp.com",
-    databaseURL: "https://fir-auth-9c195.firebaseio.com",
-    projectId: "fir-auth-9c195",
-    storageBucket: "fir-auth-9c195.appspot.com",
-    messagingSenderId: "669162847387"
-  }
+  production: false
 };
 
 /*
